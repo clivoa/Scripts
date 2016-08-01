e002db869524c90b18f2166e767aed7a9bc44bd9
Caio <cviniciuslopes@live.com>
2016-08-01 16:04:27 -0300

    Create program.py

---
+++ Python/program.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+print "Curso de pentest profissional"
+
+porta = 80
+ip = "192.168.0.1"
+ipp = raw_input("Digite seu ip ")
+portaa = input("Digite a porta ")
+#porta = int(raw_input("Digite a porta"))
+print "Varrendo host", ip, "na porta", porta
+print "Varrendo host", ipp, "na porta", portaa
