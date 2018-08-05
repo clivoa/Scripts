2e19920620a4b0a8b3eb23373f72a5490b12b27a
Caio <caioviniciuslopes@gmail.com>
2018-08-05 16:55:27 -0300

    Update parseurl.sh

--- Shell-Script/parseurl.sh
+++ Shell-Script/parseurl.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ "$1" == "" ]
-then
+if [ "$1" == "" ] then
 	echo "Analisador de URL - Tarefa Modulo 3"
 	echo "Exemplo de uso: $0 sitealvo.com.br"
 else
 	wget -q $1;
-	grep href index.html | cut -d "/" -f3 | grep "\." | cut -d '"' -f1 | sort -u | egrep -v "<li| |>" > lista.txt;
-	for x in $(cat lista.txt); do
+	grep -Eoi '<a [^>]+>' index.html | grep -Eo 'href="[^\"]+"' | grep -Eo '(http|https)://[^/"]+' | cut -d "/" -f3 | sort | uniq > lista.txt
+    	for x in $(cat lista.txt); do
 		host $x; done
 	fi
 rm -rf index.html lista.txt
