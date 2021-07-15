c03bb212a8bc5446349c03eba62bdf637743a4e3
Caio <caioviniciuslopes@gmail.com>
2021-07-15 16:05:40 -0300

    Delete pravailAddBlacklistIP.sh

--- Shell-Script/pravailAddBlacklistIP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-url_para_bloquear=`cat $1`
-
-for i in ${url_para_bloquear}
-do
-    echo "curl -s -k -X POST -H 'X-Arbux-APIToken: XXXXXXXX' -H 'Accept : application/json' https://172.18.128.150/api/aps/v1/protection-groups/blacklisted-domains/?domain="${i} | sh
-    sleep 2s
-done
